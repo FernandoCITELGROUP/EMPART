f5eb44237bd0fce55ca96b4202c9814830b83f92
Fernando <scriviafernando@gmail.com>
2019-05-09 14:49:46 +0200

    search

--- Empart/CercaViewController.swift
+++ Empart/CercaViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
-class CercaViewController: UIViewController {
-
+class CercaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    var searching = false
+    var searchInfo = [String]()
+    
+    let tableViewData = ["Roma","Napoli","Milano","Torino","Palermo","Città del Vaticano","Musei Capitolini","Museo di Capodimonte", "Real Bosco di Capodimonte", "Musei Vaticani", "Museo Egizio", "Museo dell'Automobile", "Mole Antonelliana", "Colosseo", "Colle Palatino", "Napoli Sotterranea", "Duomo di Milano", "Scuderie del Quirinale", "Museo della Scienza", "Museo del Cinema", "Mostra Pixar"]
+    
+    //    Outlets
+    @IBOutlet weak var tableViewOne: UITableView!
+    @IBOutlet weak var searchBarOne: UISearchBar!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if searching{
+            return searchInfo.count
+        }else {
+            return tableViewData.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath)
+        
+        if searching {
+            cell.textLabel?.text = searchInfo[indexPath.row]
+        }else{
+            cell.textLabel?.text = tableViewData[indexPath.row]
+        }
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableViewOne.delegate = self
+        self.tableViewOne.dataSource = self
+        self.searchBarOne.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchInfo = tableViewData.filter({$0.prefix(searchText.count) == searchText})
+        searching = true
+        tableViewOne.reloadData()
     }
-    */
-
 }
