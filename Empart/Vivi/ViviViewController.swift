6406ddc55b26ea5fd64164366b46cb9e0df2a2ef
Fernando <scriviafernando@gmail.com>
2019-05-10 11:28:21 +0200

    vivi

--- Empart/Vivi/ViviViewController.swift
+++ Empart/Vivi/ViviViewController.swift
@@ -10,23 +10,21 @@ import UIKit
 
 class ViviViewController: UIViewController {
 
+    // Outlets
     @IBOutlet weak var viviPulseView: UIView!
-    
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var tourMapButton: UIButton!
     
+    
+    // Attributes for animation
     var pulse : Pulsing!
     var pulse2 : Pulsing!
-    
+    var timer = Timer()
+   
+    // Attribute
     var liveTour:TourEmpart!
-    
-    @IBAction func backButtonAction(_ sender: Any) {
-        
-        self.pulse.removeFromSuperlayer()
-        self.pulse2.removeFromSuperlayer()
-        self.navigationController?.popViewController(animated: true)
-    }
-    
+  
+    // Events
     override func viewDidLoad() {
         super.viewDidLoad()
         self.viviPulseView.backgroundColor = UIColor.purple
@@ -34,14 +32,43 @@ class ViviViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         self.addPulse()
+        self.scheduledTimerWithTimeInterval()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.timer.invalidate()
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+
+    // Functions
+    
+    func scheduledTimerWithTimeInterval(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.pulseFunction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func pulseFunction(){
+        self.addPulse()
+    }
+
     func addPulse()
     {
-        self.pulse = Pulsing(numberOfPulses: 50, radius: 300, position: self.viviPulseView.center)
+        if(self.pulse != nil && self.pulse2 != nil){
+            self.pulse.removeFromSuperlayer()
+            self.pulse2.removeFromSuperlayer()
+        }
+        
+        self.pulse = Pulsing(numberOfPulses: 1, radius: 300, position: self.viviPulseView.center)
         self.pulse.animationDuration = 0.8
         self.pulse.backgroundColor = UIColor.purple.cgColor
-        self.pulse2 = Pulsing(numberOfPulses: 50, radius: 600, position: self.viviPulseView.center)
+        self.pulse2 = Pulsing(numberOfPulses: 1, radius: 600, position: self.viviPulseView.center)
         self.pulse2.animationDuration = 0.8
         self.pulse2.backgroundColor = UIColor.purple.cgColor
         
@@ -50,30 +77,11 @@ class ViviViewController: UIViewController {
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    // Actions
+    @IBAction func backButtonAction(_ sender: Any) {
         
-        // Hide the navigation bar on the this view controller
-        self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        self.tabBarController?.tabBar.isHidden = true
+        self.pulse.removeFromSuperlayer()
+        self.pulse2.removeFromSuperlayer()
+        self.navigationController?.popViewController(animated: true)
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Show the navigation bar on other view controllers
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
-        self.tabBarController?.tabBar.isHidden = false
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
